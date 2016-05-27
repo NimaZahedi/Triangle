bd1149fcd6cfea5cb5e03e302e6805402ad002a8
NimaZahedi <Nima.Zahedee@gmail.com>
2016-05-27 21:09:08 +0200

    import and use triangle-class in main

--- app/main.js
+++ app/main.js
@@ -1,10 +1,21 @@
 import {getType} from "./triangle";
+import {Triangle} from "./triangle-class";
+
+
 
 const args = process.argv[2].split(',');
 const sides = [];
 
 for (let arg of args) {
     sides.push(Number(arg));
+}
+
+try {
+    const tr = new Triangle(sides);
+    console.log(tr.getType());
+}
+catch (e) {
+    console.log(`Error Name: ${e.name}, message: ${e.message}`);
 }
 
 try {
